861885313021f163481af0af7cc7d60d835f9d30
paripasd <paripasd@gmail.com>
2022-05-06 19:14:01 +0200

    Update Tournament.java

--- src/ModelLayer/Tournament.java
+++ src/ModelLayer/Tournament.java
@@ -1,5 +1,68 @@
 package ModelLayer;
 
-public class Tournament {
+import java.util.*;
 
+public class Tournament {
+	
+	private int tournamentID;
+	private Date date;
+	private String venue;
+	private String tournamentName;
+	private boolean isInProgress;
+	private boolean isFinished;
+	
+	private Tournament(int tournamentID, Date date, String venue, String tournamentName) {
+		this.tournamentID = tournamentID;
+		this.date = date;
+		this.venue = venue;
+		this.tournamentName = tournamentName;
+	}
+
+	public int getTournamentID() {
+		return tournamentID;
+	}
+
+	public void setTournamentID(int tournamentID) {
+		this.tournamentID = tournamentID;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getVenue() {
+		return venue;
+	}
+
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+
+	public String getTournamentName() {
+		return tournamentName;
+	}
+
+	public void setTournamentName(String tournamentName) {
+		this.tournamentName = tournamentName;
+	}
+
+	public boolean isInProgress() {
+		return isInProgress;
+	}
+
+	public void setInProgress(boolean isInProgress) {
+		this.isInProgress = isInProgress;
+	}
+
+	public boolean isFinished() {
+		return isFinished;
+	}
+
+	public void setFinished(boolean isFinished) {
+		this.isFinished = isFinished;
+	}
 }
