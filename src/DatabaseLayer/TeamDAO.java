e833759be9651951822b2e2c839709dd5ec1f25c
kir40lmao <kirilsyuleyman@gmail.com>
2022-05-08 23:46:26 +0200

    Fix
    because I did an oopsie and did TournnamentDAO instead of Team Dao

--- src/DatabaseLayer/TeamDAO.java
+++ src/DatabaseLayer/TeamDAO.java
@@ -2,31 +2,32 @@ package DatabaseLayer;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TeamDAO implements TeamDAOIF{
 	DBConnection dbcon = DBConnection.getInstance();
 	Connection con = dbcon.getDBcon();
 	
 	@Override
-	public void createTournament(String name) {
-		String sql = "INSERT INTO Tournament(TournamentID, [Tournament Name], [Date], Venue, Status) VALUES(?, ?, ?, ?, ?)";
+	public void createTeam(String name) {
+		String sql = "INSERT INTO Teams(TeamID,TeamName, Wins, Loses) VALUES(?, ?, ?, ?)";
 		
 		PreparedStatement statement;
 		try{
-			String venue = "";
-			String status = "";
+			int id = 0;
 			statement = con.prepareStatement(sql);
-			//statement.setInt(); will we have to read all IDs within database to get the next ID or is there some other way??
-			statement.setString(0, name);
-			statement.setDate(0, null, null); //research which method is best for inserting date data in MSSQL
-			statement.setString(0, venue);
-			statement.setString(0, status);
+			statement.setInt(1, id);
+			statement.setString(2, name);
+			statement.setInt(3, 0);
+			statement.setInt(4, 0);
 			
 			
 			int rowsInserted = statement.executeUpdate();
 			if (rowsInserted > 0) {
-			    System.out.println("Tournament created");
+			    System.out.println("Team created");
+			    //I know this shouldn't be here, just might use it for testing purposes later.
 			}
 		}catch(SQLException e){
 			e.printStackTrace();
@@ -34,39 +35,60 @@ public class TeamDAO implements TeamDAOIF{
 		}
 
 	@Override
-	public void findTournament(int tournamentID) {
-		// TODO Auto-generated method stub
+	public void findTeamByID(int teamID) {
+		try {	
+			String sql = "SELECT * FROM Item WHERE ID = '"+teamID+"'";
+			 
+			Statement statement = con.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			 
+			//can put while expression within a higher order function and use lambdas to avoid code duplication
+			//(will have to use same code in all get methods) - discuss with group
+			while (result.next()){
+			    int id = result.getInt("TeamID");
+			    String teamName = result.getString("TeamName");
+			    int wins = result.getInt("Wins");
+			    int loses = result.getInt("Loses");
+
+
+			    //testing purposes, REMOVE LATER
+			    String output = "Team #%d: %d - %s - %s - %s - %s";
+			    System.out.println(String.format(output, id,teamName,wins,loses));
+			}
+		     
+		} catch (SQLException ex) {
+		    ex.printStackTrace();
+		}
 		
 	}
 
 	@Override
-	public void getAllTournaments() {
-		// TODO Auto-generated method stub
-		
+	public void findAvailiableTeams() {
+		//higher order function + lambda could be used here;
+		//maybe change to getElidgeable(Teams with at least 5 players) since teams will always be available
+	}
+	
+	public void getAllTeams() {
+		//higher order function + lambda could be used here;
+		//maybe change to getElidgeable(Teams with at least 5 players) since teams will always be available
 	}
 
 	@Override
-	public void getUpcoming() {
-		// TODO Auto-generated method stub
+	public void updateTeamName(int teamID, String name) {
+		String sql ="UPDATE Team WHERE ID = '"+teamID+"'";
 		
-	}
-
-	@Override
-	public void getOngoing() {
-		// TODO Auto-generated method stub
+		int wins = 0;
+		int loses = 0;
+		PreparedStatement statement;
+		//what CAN we update?
+		try {
+		statement = con.prepareStatement(sql);
+		statement.setString(0, name);
+		statement.setInt(4, wins);
+		statement.setInt(5, loses);
 		
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
-	@Override
-	public void updateTournament(int tournamentID) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteTournament(int tournamentID) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
--- src/DatabaseLayer/TeamDAOIF.java
+++ src/DatabaseLayer/TeamDAOIF.java
@@ -1,13 +1,11 @@
 package DatabaseLayer;
 
 public interface TeamDAOIF {
-	void createTournament(String name);
-	void findTournament(int tournamentID);
-	void getAllTournaments();
-	void getUpcoming();
-	void getOngoing();
-	//void getFinished(); - not in DCD; could maybe just be UI method
-	void updateTournament(int tournamentID);
-	void deleteTournament(int tournamentID);
+
+	void createTeam(String name);
+	void findTeamByID(int teamID);
+	void findAvailiableTeams();
+	void getAllTeams();
+	void updateTeamName(int teamID, String name);
 	
 }
