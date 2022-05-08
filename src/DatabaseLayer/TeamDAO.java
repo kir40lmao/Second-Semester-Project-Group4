ad851303335b2870077e19ce2054acdd22c7756e
kir40lmao <kirilsyuleyman@gmail.com>
2022-05-08 22:23:39 +0200

    Light TeamDAO work
    don't want to lose it again so I'm saving it lol

--- src/DatabaseLayer/TeamDAO.java
+++ src/DatabaseLayer/TeamDAO.java
@@ -1,5 +1,72 @@
 package DatabaseLayer;
 
-public class TeamDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TeamDAO implements TeamDAOIF{
+	DBConnection dbcon = DBConnection.getInstance();
+	Connection con = dbcon.getDBcon();
+	
+	@Override
+	public void createTournament(String name) {
+		String sql = "INSERT INTO Tournament(TournamentID, [Tournament Name], [Date], Venue, Status) VALUES(?, ?, ?, ?, ?)";
+		
+		PreparedStatement statement;
+		try{
+			String venue = "";
+			String status = "";
+			statement = con.prepareStatement(sql);
+			//statement.setInt(); will we have to read all IDs within database to get the next ID or is there some other way??
+			statement.setString(0, name);
+			statement.setDate(0, null, null); //research which method is best for inserting date data in MSSQL
+			statement.setString(0, venue);
+			statement.setString(0, status);
+			
+			
+			int rowsInserted = statement.executeUpdate();
+			if (rowsInserted > 0) {
+			    System.out.println("Tournament created");
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		}
+
+	@Override
+	public void findTournament(int tournamentID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getAllTournaments() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getUpcoming() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getOngoing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTournament(int tournamentID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTournament(int tournamentID) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/DatabaseLayer/TeamDAOIF.java
+++ src/DatabaseLayer/TeamDAOIF.java
@@ -1,5 +1,13 @@
 package DatabaseLayer;
 
 public interface TeamDAOIF {
-
+	void createTournament(String name);
+	void findTournament(int tournamentID);
+	void getAllTournaments();
+	void getUpcoming();
+	void getOngoing();
+	//void getFinished(); - not in DCD; could maybe just be UI method
+	void updateTournament(int tournamentID);
+	void deleteTournament(int tournamentID);
+	
 }
