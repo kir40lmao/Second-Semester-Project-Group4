127dbc6b9ec365a07a0c2b69dab23e3ca31989a1
rHidden <davidbuncek@gmail.com>
2022-05-17 12:23:41 +0200

    DBConnection for real, kiril dont update it anymore pls ty

--- src/DatabaseLayer/DBConnection.java
+++ src/DatabaseLayer/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 
 
 public class DBConnection {   
-
+	//Constants used to get access to the database
 	
 	private static final String  driver = "jdbc:sqlserver://hildur.ucn.dk\\SQLExpress:1433;";
     private static final String  databaseName = ";databaseName=CSC-CSD-S212_10407567;";
@@ -17,21 +17,16 @@ public class DBConnection {
     private DatabaseMetaData dma;
     private static Connection con;
     
-
-
+    // an instance of the class is generated
     private static DBConnection  instance = null;
 
-
-
-    private static DBConnection  instance = null;
-
-
+    // the constructor is private to ensure that only one object of this class is created
     private DBConnection()
     {
     	String url = driver + databaseName + userName + password;
 
         try{
-
+            //load of driver
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             System.out.println("Driver class loaded ok");
           
@@ -41,32 +36,22 @@ public class DBConnection {
             System.out.println(e.getMessage());
         }
         try{
-
+            //connection to the database
             con = DriverManager.getConnection(url);
             con.setAutoCommit(true);
-            dma = con.getMetaData();
+            dma = con.getMetaData(); // get meta data
             System.out.println("Connection to " + dma.getURL());
             System.out.println("Driver " + dma.getDriverName());
             System.out.println("Database product name " + dma.getDatabaseProductName());
-
-            con = DriverManager.getConnection(url);
-            con.setAutoCommit(true);
-            dma = con.getMetaData();
-            System.out.println("Connection to " + dma.getURL());
-            System.out.println("Driver " + dma.getDriverName());
-            System.out.println("Database product name " + dma.getDatabaseProductName());
-        }
-
+        }//end try
         catch(Exception e){
             System.out.println("Problems with the connection to the database:");
             System.out.println(e.getMessage());
             System.out.println(url);
-
-        }
-    }
-
-        }
-    }
+        }//end catch
+    }//end  constructor
+	   
+  //closeDb: closes the connection to the database
     public static void closeConnection()
     {
        	try{
@@ -77,25 +62,19 @@ public class DBConnection {
          catch (Exception e){
             System.out.println("Error trying to close the database " +  e.getMessage());
          }
-
-    }
+    }//end closeDB
 		
-    
-
-    }
-
+    //getDBcon: returns the singleton instance of the DB connection
     public Connection getDBcon()
     {
        return con;
     }
-
+    //getDBcon: returns the singleton instance of the DB connection
     public static boolean instanceIsNull()
     {
        return (instance == null);
     }    
-
-
-
+    //this method is used to get the instance of the connection
     public static DBConnection getInstance()
     {
         if (instance == null)
@@ -105,7 +84,5 @@ public class DBConnection {
         return instance;
     }
 
-
-}
-}
+}//end DbConnection
 
